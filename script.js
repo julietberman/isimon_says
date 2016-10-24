c6c3c15214b270e14fbfa4fc5dd75f5a17bd3b1e
Juliet Berman <berman.juliet@gmail.com>
2016-10-24 14:06:40 -0400

    add point system and final score feature

--- script.js
+++ script.js
@@ -7,15 +7,20 @@ simon:[],
 player: []
 }
 
+var counter = null;
 
+// Click event for begin game button triggers the random simon sequence to begin, the score board to populate, and timer function to initiate
 
 $("button").on("click", function(){
   setTimeout(simonSequence, 1000);
   scoreBoard();
-  timer();
+  counter = setInterval(timer, 1000);
 });
 
+// Letter buttons triggers player sequence to capture and store player values in an array and compare to simon sequence values
 $(".simon").on("click", playerSequence);
+
+// simon sequence randomly generates letter to flash and stores sequence value
 
 function simonSequence (){
   var number = 	Math.floor( Math.random() * 5)
@@ -25,10 +30,9 @@ function simonSequence (){
         $("img#" + color).fadeOut(200).fadeIn(200);
       }, 800 * i)
     });
-    console.log(gameData.simon)
 }
 
-
+// player sequence function captures and stores player values in an array and compares to simon sequence values
 function playerSequence(){
   $(this).fadeOut(200).fadeIn(200);
   gameData.player.push(($(this).attr("id")))
@@ -41,7 +45,7 @@ function playerSequence(){
   }
 };
 
-
+// if simon data and player data have the same number of values, a level is added, simon sequence is triggered again, and player data is cleared for the next level
 function checkMatch(){
   var x = true;
   gameData.simon.forEach(function(value, index){
@@ -51,7 +55,7 @@ function checkMatch(){
   })
 if (x == false){
   setTimeout(changeBackground, 350)
-  // gameOver();
+  setTimeout(gameOver, 350);
 
 }
 else {
@@ -61,30 +65,34 @@ else {
   gameData.player=[];
 };
 
+// triggered if there is no match between player and simon values
 function changeBackground(){
   $("body").addClass("false").removeClass("true");
   console.log("wrong")
 };
 
+// generates score board upon begin game
 function scoreBoard(){
-  $("#mainText").html(`<div id='scoreBoard'>level: 1</div><div id='timer'>timer: 0</div>`).css('border-top', '4px dotted white');
+  $("#mainText").html(`<div id='scoreBoard'>level: 1</div><div id='timer'>timer: 0 sec</div>`).css('border-top', '4px dotted white');
 };
 
+// timer feature
 var seconds = 0;
 function timer(){
     $("#timer").html(`timer: ${seconds} sec`);
     seconds++;
-    console.log(seconds);
-    setTimeout(timer, 1000);
+
 }
 
-var x = 1;
+// level feature
+var level = 1;
 function addLevel(){
-  x++;
-  $("#scoreBoard").html(`level: ${x}`)
+  level++;
+  $("#scoreBoard").html(`level: ${level}`)
 };
 
-// function gameOver(){
-//   simon=[];
-//   player=[];
-// }
+function gameOver(){
+  var total = level * seconds
+  clearInterval(counter);
+  $("#mainText").html(`<p>final score:<br/><span class='total'>(level ${level} *  ${seconds} sec)</span>:</p><p id = "total">${total} points</p>`);
+}
